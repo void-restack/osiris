060e25630710a754d7d1cd2b4ab5cd15d9c8bdb4
void-restack <rahul@fetcch.xyz>
2025-06-27 16:25:57 +0530

    chore: initial setup

---
+++ apps/web/src/routes/__root.tsx
@@ -0,0 +1,51 @@
+import {
+	createRootRouteWithContext,
+	HeadContent,
+	Outlet,
+} from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
+import "../index.css";
+
+export type RouterAppContext = {};
+
+export const Route = createRootRouteWithContext<RouterAppContext>()({
+	component: RootComponent,
+	head: () => ({
+		meta: [
+			{
+				title: "Osiris",
+			},
+			{
+				name: "description",
+				content: "Osiris",
+			},
+		],
+		links: [
+			{
+				rel: "icon",
+				href: "/favicon.ico",
+			},
+		],
+	}),
+});
+
+function RootComponent() {
+	// const isFetching = useRouterState({
+	//   select: (s) => s.isLoading,
+	// });
+
+	return (
+		<>
+			<HeadContent />
+			<ThemeProvider defaultTheme="light" storageKey="osiris-ui-theme">
+				<div className="grid h-svh grid-rows-[auto_1fr]">
+					<Outlet />
+				</div>
+				<Toaster richColors />
+			</ThemeProvider>
+			<TanStackRouterDevtools position="bottom-left" />
+		</>
+	);
+}
