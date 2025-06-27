2144053b31a9429153708ec514b2d0508c11a799
void-restack <rahul@fetcch.xyz>
2025-06-27 16:35:00 +0530

    chore: added typescript in root

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,12 +1,11 @@
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import ReactDOM from "react-dom/client";
-import Loader from "./components/loader";
 import { routeTree } from "./routeTree.gen";
 
 const router = createRouter({
 	routeTree,
 	defaultPreload: "intent",
-	defaultPendingComponent: () => <Loader />,
+	defaultPendingComponent: () => <div>Loading...</div>,
 	context: {},
 });
 
