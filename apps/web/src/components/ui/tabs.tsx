653b9008f92f5ccc99225a4733c14b31a7dc5591
chann44 <44chansw@gmail.com>
2025-06-28 17:02:54 +0530

    tabs added

---
+++ apps/web/src/components/ui/tabs.tsx
@@ -0,0 +1,68 @@
+import * as React from "react"
+import * as TabsPrimitive from "@radix-ui/react-tabs"
+
+import { cn } from "@/lib/utils"
+
+function Tabs({
+  className,
+  ...props
+}: React.ComponentProps<typeof TabsPrimitive.Root>) {
+  return (
+    <TabsPrimitive.Root
+      data-slot="tabs"
+      className={cn("flex flex-col gap-2", className)}
+      {...props}
+    />
+  )
+}
+
+function TabsList({
+  className,
+  ...props
+}: React.ComponentProps<typeof TabsPrimitive.List>) {
+  return (
+    <TabsPrimitive.List
+      data-slot="tabs-list"
+      className={cn(
+        "bg-transparent text-muted-foreground inline-flex h-9 w-fit items-center justify-center rounded-lg p-[3px]",
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+function TabsTrigger({
+  className,
+  children,
+  ...props
+}: React.ComponentProps<typeof TabsPrimitive.Trigger>) {
+  return (
+    <TabsPrimitive.Trigger
+      data-slot="tabs-trigger"
+      className={cn(
+        "tab-trigger top-[1px]   h-12 px-8 justify-start data-[state=active]:bg-transparent relative text-primary-300 dark:data-[state=active]:text-primary-800 data-[state=active]:text-primary-800 focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring dark:data-[state=active]:border-input dark:data-[state=active]:bg-input/30  dark:text-muted-foreground inline-flex w-max items-center gap-1.5 rounded-md border border-transparent py-1 text-sm font-medium whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50  [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 data-[state=active]:[&_svg]:text-success-500",
+        className
+      )}
+      {...props}
+    >
+      {children}
+      <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-4/5 h-px bg-success-500 rounded-full opacity-0 data-[state=active]:opacity-100" />
+    </TabsPrimitive.Trigger>
+  )
+}
+
+function TabsContent({
+  className,
+  ...props
+}: React.ComponentProps<typeof TabsPrimitive.Content>) {
+  return (
+    <TabsPrimitive.Content
+      data-slot="tabs-content"
+      className={cn("flex-1 outline-none", className)}
+      {...props}
+    />
+  )
+}
+
+export { Tabs, TabsList, TabsTrigger, TabsContent }
--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,77 +1,20 @@
 import { McpSearchBox } from "@/components/features/mcp-search/mcp-search-box";
 import { createFileRoute } from "@tanstack/react-router";
-import { McpVersions } from "@/components/features/mcp-details/mcp-versions";
-import { InstallAction } from "@/components/features/mcp-details/install-action";
 import { McpDetailsHeader } from "@/components/features/mcp-details/details-header";
-import { capabilities, McpCapabilitiesList } from "@/components/features/mcp-details/capabilites-list";
-import { ServerList, type McpServer } from "@/components/features/mcp-details/mcp-servers";
-import { McpActionTable } from "@/components/features/mcp-details/mcp-action-table";
-import { authenticators, McpAuthList } from "@/components/features/mcp-details/mcp-auth";
+import { McpTabs } from "@/components/features/mcp-details/mcp-details-tab";
 
 export const Route = createFileRoute("/")({
 	component: HomeComponent,
 });
 
-const servers: McpServer[] = [
-	{
-		name: "Server 1",
-		createdAt: "2021-01-01",
-		apps: [
-			{
-				icon: "/test/gmail.svg",
-				name: "Gmail",
-				scopes: 1,
-				isAuthenticated: false,
-			},
-			{
-				icon: "/test/calander.svg",
-				name: "Calendar",
-				scopes: 1,
-				isAuthenticated: false,
-			},
-			{
-				icon: "/test/contact.svg",
-				name: "Contact",
-				scopes: 10,
-				isAuthenticated: true,
-			},
-		],
-	},
-	{
-		name: "Server 1",
-		createdAt: "2021-01-01",
-		apps: [
-			{
-				icon: "/test/gmail.svg",
-				name: "Gmail",
-				scopes: 1,
-				isAuthenticated: false,
-			},
-			{
-				icon: "/test/calander.svg",
-				name: "Calendar",
-				scopes: 1,
-				isAuthenticated: false,
-			},
-			{
-				icon: "/test/contact.svg",
-				name: "Contact",
-				scopes: 10,
-				isAuthenticated: true,
-			},
-		],
-	},
-];
 
 function HomeComponent() {
 	return <div>
 		<McpSearchBox />
 		<McpDetailsHeader />
 		{/* <McpCapabilitiesList data={capabilities} /> */}
-		<div className="px-10">
-		<ServerList data={servers} />
-		<McpAuthList data={authenticators} />
-		<McpActionTable />
+		<div className=" w-full">
+			<McpTabs />
 		</div>
 	</div>;
 }
