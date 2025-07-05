db1f549d7977f92a5fab2222218f2f74255264ae
Satyam Kulkarni <punekar.satyam@gmail.com>
2025-07-05 21:06:41 +0530

    Feat: .

--- apps/docs/src/lib/source.ts
+++ apps/docs/src/lib/source.ts
@@ -4,6 +4,6 @@ import { docs } from "@/.source";
 // See https://fumadocs.vercel.app/docs/headless/source-api for more info
 export const source = loader({
 	// it assigns a URL to your pages
-	baseUrl: "/docs",
+	baseUrl: "/",
 	source: docs.toFumadocsSource(),
 });
