223e9723f88e9b90826f3bc0c7fd5d4f577b91b6
chann44 <44chansw@gmail.com>
2025-07-13 17:32:13 +0530

    chore:  just removed unused imports

--- apps/web/src/routes/hub/mcp.$mcpId.tsx
+++ apps/web/src/routes/hub/mcp.$mcpId.tsx
@@ -1,24 +1,12 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { McpDetailsHeader } from "@/components/features/mcp-details/details-header";
 import { McpTabs } from "@/components/features/mcp-details/mcp-details-tab";
-import {
-	Breadcrumb,
-	BreadcrumbItem,
-	BreadcrumbLink,
-	BreadcrumbList,
-	BreadcrumbPage,
-	BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
-import { Separator } from "@/components/ui/separator";
-import { SidebarTrigger } from "@/components/ui/sidebar";
 
 export const Route = createFileRoute("/hub/mcp/$mcpId")({
 	component: SlugComponent,
 });
 
 function SlugComponent() {
-	const { slug } = Route.useParams();
-
 	return (
 		<main className="flex h-full flex-col gap-4">
 			<McpDetailsHeader />
