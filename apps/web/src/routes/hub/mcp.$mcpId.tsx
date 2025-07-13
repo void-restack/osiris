1c55dfff6a10ebbe39fd4f344c2b98897e307b26
chann44 <44chansw@gmail.com>
2025-07-13 17:31:15 +0530

    added mcp into correct places

--- apps/web/src/routes/hub/mcp.$mcpId.tsx
+++ apps/web/src/routes/hub/mcp.$mcpId.tsx
@@ -1,9 +1,28 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { McpDetailsHeader } from "@/components/features/mcp-details/details-header";
+import { McpTabs } from "@/components/features/mcp-details/mcp-details-tab";
+import {
+	Breadcrumb,
+	BreadcrumbItem,
+	BreadcrumbLink,
+	BreadcrumbList,
+	BreadcrumbPage,
+	BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import { SidebarTrigger } from "@/components/ui/sidebar";
 
 export const Route = createFileRoute("/hub/mcp/$mcpId")({
-	component: RouteComponent,
+	component: SlugComponent,
 });
 
-function RouteComponent() {
-	return <div>Hello "/hub/mcp/$mcpId"!</div>;
+function SlugComponent() {
+	const { slug } = Route.useParams();
+
+	return (
+		<main className="flex h-full flex-col gap-4">
+			<McpDetailsHeader />
+			<McpTabs />
+		</main>
+	);
 }
--- apps/web/src/routes/hub/mcp.index.tsx
+++ apps/web/src/routes/hub/mcp.index.tsx
@@ -1,14 +1,45 @@
+import { useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
+import { z } from "zod";
+import { McpListContainer } from "@/components/features/mcp-list/mcp-list-container";
+import { McpSearchBox } from "@/components/features/mcp-search/mcp-search-box";
+import { packageQueries } from "@/lib/queries";
+
+const packagesSearchSchema = z.object({
+	page: z.number().optional().default(1),
+	search: z.string().optional(),
+	publisherId: z.string().optional(),
+});
 
 export const Route = createFileRoute("/hub/mcp/")({
+	validateSearch: packagesSearchSchema,
+	loaderDeps: ({ search }) => ({ search }),
+	loader: ({ context: { queryClient }, deps }) =>
+		queryClient.ensureQueryData(
+			packageQueries.listOptions({
+				page: 0, //deps.search.page,
+				name: "", //deps.search.search,
+				publisherId: "4beb08f3-a8d7-4610-987b-67763b4b8672", //deps.search.publisherId,
+			}),
+		),
 	component: RouteComponent,
 });
 
 function RouteComponent() {
+	const publisherId = "4beb08f3-a8d7-4610-987b-67763b4b8672";
+	const page = 0;
+	const search = "";
+	const { data: packages } = useSuspenseQuery(
+		packageQueries.listOptions({
+			page,
+			name: search,
+			publisherId,
+		}),
+	);
 	return (
-		<div>
-			<h1>Packages</h1>
-			<div className="grid gap-4" />
-		</div>
+		<main className="flex h-full flex-col">
+			<McpSearchBox />
+			<McpListContainer />
+		</main>
 	);
 }
