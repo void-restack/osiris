19e8a917b4c33f6a9a23d071aca9d6d830a7d81e
void-restack <rahul@fetcch.xyz>
2026-02-09 13:29:06 +0530

    chore: change redirects

--- apps/web/src/routes/login.tsx
+++ apps/web/src/routes/login.tsx
@@ -18,7 +18,7 @@ function RouteComponent() {
   const handleGoogleLogin = () => {
     setIsGoogleLoading(true)
     loginMutation.mutate(
-      { provider: 'google', redirectUri: "https://osiris-web-git-profile-restacks-projects-1987245d.vercel.app/auth" },
+      { provider: 'google', redirectUri: "https://app.osirislabs.xyz/auth" },
       {
         onSuccess: () => {
           setIsGoogleLoading(false)
@@ -34,7 +34,7 @@ function RouteComponent() {
   const handleGithubLogin = () => {
     setIsGithubLoading(true)
     loginMutation.mutate(
-      { provider: 'github', redirectUri: "https://osiris-web-git-profile-restacks-projects-1987245d.vercel.app/auth" },
+      { provider: 'github', redirectUri: "https://app.osirislabs.xyz/auth" },
       {
         onSuccess: () => {
           setIsGithubLoading(false)
